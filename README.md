9094a37adadcf03657587677f260ec0e26b28922
Tom Flídr <tomflidr@gmail.com>
2018-11-05 15:14:33 +0100

    config refactoring

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# ext-config-yaml
+# MvcCore Extension - Config - YAML
+
+[![Latest Stable Version](https://img.shields.io/badge/Stable-v4.3.1-brightgreen.svg?style=plastic)](https://github.com/mvccore/ext-config-yaml/releases)
+[![License](https://img.shields.io/badge/Licence-BSD-brightgreen.svg?style=plastic)](https://mvccore.github.io/docs/mvccore/4.0.0/LICENCE.md)
+![PHP Version](https://img.shields.io/badge/PHP->=5.3-brightgreen.svg?style=plastic)
+
 Yaml configuration files syntax.
+
+## Installation
+```shell
+composer require mvccore/ext-config-yaml
+```
+
+## Usage
+Add this to `Bootstrap.php` or to very application beginning:
+```php
+\MvcCore\Ext\Auth::GetInstance()->SetConfigClass('MvcCore\Ext\Config\Yaml');
+```
---
+++ src/MvcCore/Ext/Configs/Yaml.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * MvcCore
+ *
+ * This source file is subject to the BSD 3 License
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that are distributed with this source code.
+ *
+ * @copyright	Copyright (c) 2016 Tom Flídr (https://github.com/mvccore/mvccore)
+ * @license		https://mvccore.github.io/docs/mvccore/4.0.0/LICENCE.md
+ */
+
+namespace MvcCore\Ext\Configs;
+
+/**
+ */
+class Yaml implements \MvcCore\IConfig
+{
+	use \MvcCore\Config\Environment;
+	use \MvcCore\Config\ReadingWriting;
+	use \MvcCore\Config\Environment;
+	use \MvcCore\Ext\Configs\Yamls\ReadingYaml;
+	use \MvcCore\Ext\Configs\Yamls\WritingYaml;
+	
+	public static $SystemConfigPath = '/%appPath%/config.yaml';
+}
